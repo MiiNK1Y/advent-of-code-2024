6351d578241dd918097bb5ca14989e00255e4726
MiiNK1Y <98641699+MiiNK1Y@users.noreply.github.com>
2024-12-04 23:26:16 +0100

    cleanup

--- day-4/solution_p2.py
+++ day-4/solution_p2.py
@@ -1,10 +1,10 @@
 class seeker:
     def __init__(self, input: str, match: list[str]) -> None:
         self._match = match
+        self._width_of_match = max(len(i) for i in self._match)
         self._input_lines = input.splitlines()
         self._height = len(self._input_lines)
         self._width = max(len(i) for i in self._input_lines)
-        self._width_of_match = max(len(i) for i in self._match)
 
         self._count = 0
 
@@ -13,11 +13,11 @@ class seeker:
     def get_count(self) -> int:
         return self._count
 
-    def _modify_derivative(self, derivate: str) -> str:
+    def _modify_derivative(self, derivative: str) -> str:
         modified_string = ""
-        for i in range(len(derivate)):
+        for i in range(len(derivative)):
             if i % 2 == 0:
-                modified_string += derivate[i] + "."
+                modified_string += derivative[i] + "."
 
         return modified_string[:-1]
 
@@ -25,13 +25,13 @@ class seeker:
         for word in self._match:
             window = 3  # x=3 y=3
 
-            for x in range(self._height):
-                derivative = self._input_lines[x : x + window]
+            for i in range(self._height):
+                derivative = self._input_lines[i : i + window]
 
                 to_match = ""
-                for i in range(self._width):
-                    for j in range(len(derivative)):
-                        to_match += derivative[j][i : i + 3]
+                for j in range(self._width):
+                    for k in range(len(derivative)):
+                        to_match += derivative[k][j : j + 3]
 
                         if (
                             len(to_match) == self._width_of_match
@@ -54,3 +54,8 @@ f.close()
 find = seeker(input, ["M.S.A.M.S", "S.M.A.S.M", "S.S.A.M.M", "M.M.A.S.S"])
 
 print(find.get_count())
+
+# PART 2:
+#
+# CORRECT ANSWER:
+# 1807
